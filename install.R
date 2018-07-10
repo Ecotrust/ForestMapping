41f124ff60722c95591a80e7174c78a352563646
sloreno <sara.loreno@gmail.com>
2018-07-10 16:11:31 -0700

    initial commit

---
+++ install.R
@@ -0,0 +1,7 @@
+install.packages("sp")
+install.packages("raster")
+install.packages("rgdal")
+install.packages("rgeos")
+install.packages("dismo")
+install.packages("maptools")
+install.packages("ggmap")
