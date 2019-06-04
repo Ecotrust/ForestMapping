4c425595be178f2095e652c488f17bb8751ae735
Ubuntu Ford <ubuntu@Ford.ecotrust.org>
2019-06-04 12:28:44 -0700

    Moving to cookiecutter data science template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predicting Pacific Northwest forest types from remotely-sensed data.',
+    author='Ecotrust',
+    license='BSD-3',
+)
